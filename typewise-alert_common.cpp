c78463238e478b97bd744532698d9fc3e5b9be80
vacuuum <47915224+vacuuum@users.noreply.github.com>
2024-11-02 12:46:26 +0530

    Update typewise-alert_common.cpp

--- typewise-alert_common.cpp
+++ typewise-alert_common.cpp
@@ -12,32 +12,31 @@ BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
   return NORMAL;
 }
 
-BreachType classifyTemperatureBreach(
-    CoolingType coolingType, double temperatureInC) {
-  int lowerLimit = 0;
-  int upperLimit = 0;
-  switch(coolingType) {
-    case PASSIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 35;
-      break;
-    case HI_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 45;
-      break;
-    case MED_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 40;
-      break;
-  }
-  return inferBreach(temperatureInC, lowerLimit, upperLimit);
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
+    int lowerLimit = 0, upperLimit = 0;
+    getCoolingLimits(coolingType, lowerLimit, upperLimit);
+    return inferBreach(temperatureInC, lowerLimit, upperLimit);
 }
 
-void checkAndAlert(
-    AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
-  BreachType breachType = classifyTemperatureBreach(
-    batteryChar.coolingType, temperatureInC
-  );
+void getCoolingLimits(CoolingType coolingType, int &lowerLimit, int &upperLimit) {
+    switch (coolingType) {
+        case PASSIVE_COOLING:
+            lowerLimit = 0;
+            upperLimit = 35;
+            break;
+        case HI_ACTIVE_COOLING:
+            lowerLimit = 0;
+            upperLimit = 45;
+            break;
+        case MED_ACTIVE_COOLING:
+            lowerLimit = 0;
+            upperLimit = 40;
+            break;
+    }
+}
+
+void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
+  BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
   switch(alertTarget) {
     case TO_CONTROLLER:
       sendToController(breachType);
